15c7025ffad93849e97e9cfd20073053036cedd7
Sebastian <Sebastiandd87@gmail.com>
2023-11-22 13:54:36 -0500

    corrected errors

--- src/app/list-pets/list-pets.component.ts
+++ src/app/list-pets/list-pets.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Pets } from "../pets";
+import {PetService} from "../pet.service";
 
 @Component({
   selector: 'app-list-pets',
   templateUrl: './list-pets.component.html',
-  styleUrls: ['./list-pets.component.css'] // Corregir el nombre de la propiedad a styleUrls
+  styleUrls: ['./list-pets.component.css']
 })
 export class ListPetsComponent {
   pets: Pets[];
 
-  constructor() {}
+  constructor(private petService:PetService) {}
 
   ngOnInit(): void {
-    this.pets = [
-      { id: 1, name: "luna", category: "Gato" ,status:"Disponible"},
-      { id: 2, name: "Sol", category: "Perro",status:"Disponible" }
-    ];
+    this.getPets();
+  }
+  private getPets(){
+    this.petService.getListOfPets().subscribe(dato=>{
+      this.pets=dato;
+    });
   }
 }
